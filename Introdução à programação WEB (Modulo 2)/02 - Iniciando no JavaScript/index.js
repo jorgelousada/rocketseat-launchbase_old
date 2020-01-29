6a2b212dafef84a8ac334268fc0f9ff1b2abe8a1
Jorge Lousada <jorgellq@gmail.com>
2020-01-28 22:37:38 -0300

    Iniciando o uso de Javascript

---
+++ Introdução à programação WEB (Modulo 2)/02 - Iniciando no JavaScript/index.js
@@ -0,0 +1,8 @@
+/* 
+    Iniciando o uso de Javascript, Apresentação do Chrome Devtools
+    uso do console.log e execução de programas com nodejs.
+*/
+
+// Constantes precisam ser inicializadas e não aceitam valores após inicialização
+const nome = 'Jorge Bootcamp';
+console.log(nome);
