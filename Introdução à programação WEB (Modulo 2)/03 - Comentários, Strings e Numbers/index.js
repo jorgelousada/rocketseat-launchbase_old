e74d66a02f5822b44b56df6d04868e7488f60936
Jorge Lousada <jorgellq@gmail.com>
2020-01-28 22:50:13 -0300

    Comentários Strings e Numbers

---
+++ Introdução à programação WEB (Modulo 2)/03 - Comentários, Strings e Numbers/index.js
@@ -0,0 +1,13 @@
+/* 
+    Criar um programa que calcula a média das notas dos alunos e exibe calculo da média.
+    Aprendemos sobre tipos de variáveis, typeof() e Template Strings
+*/
+
+const aluno01 = 'Mayk'  // Variável tipo String
+const aluno02 = "Diego" // Variável tipo String 
+const notaAluno01 = 9.8 // Variável tipo number
+
+const sobrenome = "Cerqueira"  
+const nome = `Waleska ${sobrenome}` // Template String permitem expressões embutidas
+
+console.log(typeof(notaAluno01)); // Typeof retorna o tipo de variável
