aad012e13e42551eb74916a0587cde910bc9b4ad
Jorge Lousada <jorgellq@gmail.com>
2020-01-28 22:37:06 -0300

    Apresentação sobre linguagens de programação.

---
+++ Introdução à programação WEB (Modulo 2)/01 - Iniciando com a programação web/index.js
@@ -0,0 +1,11 @@
+/* 
+    Apresentação sobre linguagens de programação.
+*/
+
+const nome = 'Mayk';
+const idade = 35;
+
+if (idade < 99){
+    return `Fala ${nome}, você está novo para programar.`
+}
+
